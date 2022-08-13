f6377e8ab3bd2e8f867a78fa388a507cb0f3443b
BerreYesilyurt <77548130+BerreYesilyurt@users.noreply.github.com>
2022-08-13 16:40:37 +0300

    Add files via upload

---
+++ tip-donusumleri/Program.cs
@@ -0,0 +1,42 @@
+﻿// See https://aka.ms/new-console-template for more information
+byte a=2;
+short b=5;
+sbyte c=4;
+
+int d=a+b+c;
+
+Console.WriteLine(d);
+
+long h=d;
+
+float i=h;
+
+string x="hello";
+char y='z';
+object z=x+y;
+
+Console.WriteLine(z);
+
+int p=4;
+
+byte l=(byte)p;
+
+int xx=5;
+string yy=xx.ToString();
+
+Console.WriteLine(yy);
+
+int sayi=10;
+string yazi="50";
+
+Console.WriteLine(sayi+Convert.ToInt32(yazi));
+
+static void ParseMethod(){
+    string yazim="10.25";
+    int rakam=Int32.Parse(yazim);
+    Console.WriteLine(rakam);
+
+
+}
+
+ParseMethod();
