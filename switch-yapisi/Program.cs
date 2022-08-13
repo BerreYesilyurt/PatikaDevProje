7500d38c842bcd3b54fadb0a969bc7be97a3a784
BerreYesilyurt <77548130+BerreYesilyurt@users.noreply.github.com>
2022-08-13 16:45:16 +0300

    Add files via upload

---
+++ switch-yapisi/Program.cs
@@ -0,0 +1,31 @@
+﻿// See https://aka.ms/new-console-template for more information
+int month=DateTime.Now.Month;
+
+
+switch (month)
+{
+
+    case 3:
+    Console.WriteLine("Mart");
+    break;
+
+    case 8:
+    Console.WriteLine("Ağustos");
+    break;
+
+    case 1:
+    Console.WriteLine("Ocak");
+    break;
+   
+}
+
+switch (month)
+{
+
+    case 10:
+    case 11:
+    case 12:
+    Console.WriteLine("Kış");
+    break;
+
+}
