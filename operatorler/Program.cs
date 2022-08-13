f9ee26289c7b1c21b52dbd0a2dba9f9be00e2a90
BerreYesilyurt <77548130+BerreYesilyurt@users.noreply.github.com>
2022-08-13 16:38:32 +0300

    Add files via upload

---
+++ operatorler/Program.cs
@@ -0,0 +1,47 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+int x=3;
+int y=4;
+
+x+=y;
+y+=x;
+
+Console.WriteLine(x+y);
+
+x/=3;
+y/=2;
+
+bool a=true;
+bool b=false;
+
+if(a && b){
+    Console.WriteLine("True");
+}
+
+if(a && !b){
+    Console.WriteLine("True");
+}
+
+if(!a && b){
+    Console.WriteLine("True");
+}
+
+int c=7;
+int d=17;
+
+bool sonuc=c>d;
+Console.WriteLine(sonuc);
+
+sonuc=d>c;
+Console.WriteLine(sonuc);
+
+int k=10;
+int l=5;
+
+Console.WriteLine(k+l);
+Console.WriteLine(k-l);
+Console.WriteLine(k*l);
+Console.WriteLine(k/l);
+Console.WriteLine(k%l);
+
+
