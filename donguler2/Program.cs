deee9ea4672f8fff7cbf31bdc03a45aedc61bdd2
BerreYesilyurt <77548130+BerreYesilyurt@users.noreply.github.com>
2022-08-13 16:48:49 +0300

    Add files via upload

---
+++ donguler2/Program.cs
@@ -0,0 +1,39 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.Write("Bir sayı giriniz");
+int sayi=int.Parse(Console.ReadLine());
+int sayac=1;
+int toplam=0;
+float ort;
+int i=1;
+
+
+/*for(int i=0;i<=sayi;i++){
+    toplam+=i;
+    sayac++;
+}
+
+ort=(toplam)/(sayac-1);*/
+
+
+
+while(i<=sayi){
+    toplam+=i;
+    sayac++;
+    i++;
+
+}
+
+ort=toplam/(sayac);
+
+Console.WriteLine("Ortalama : "+ort);
+
+
+
+string[] arabalar={"Honda","Bmw","Mercedes"};
+
+foreach (var araba in arabalar)
+{
+    Console.WriteLine(araba);
+
+}
+
