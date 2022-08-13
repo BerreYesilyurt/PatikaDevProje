4a9ee83d2a340986e628df88f41240b9a30dd7c5
BerreYesilyurt <77548130+BerreYesilyurt@users.noreply.github.com>
2022-08-13 16:43:04 +0300

    Add files via upload

---
+++ kosul-yapisi/Program.cs
@@ -0,0 +1,14 @@
+﻿// See https://aka.ms/new-console-template for more information
+int time=DateTime.Now.Hour;
+
+if(time<18){
+    Console.WriteLine("İyi Günler");
+
+}
+
+else{
+    Console.WriteLine("İyi Akşamlar");
+}
+
+
+string sonuc=time<18 ? "İyi Günler":"İyi Akşamlar";
