944ac42f455a4ebe92b939be07b359c4f2979ff4
BerreYesilyurt <77548130+BerreYesilyurt@users.noreply.github.com>
2022-08-13 16:52:01 +0300

    Add files via upload

---
+++ array-sinifi-metotlari/Program.cs
@@ -0,0 +1,26 @@
+﻿// See https://aka.ms/new-console-template for more information
+int[] rakamlar={1,2,3,4,5,6,7};
+
+Array.Sort(rakamlar);
+
+Array.Clear(rakamlar,3,3);
+
+foreach (var no in rakamlar)
+{
+    Console.Write(no);
+    Console.WriteLine(" ");
+    
+}
+
+Array.Reverse(rakamlar);
+
+foreach (var no in rakamlar)
+{
+    Console.Write(no);
+    Console.WriteLine(" ");
+    
+}
+
+Console.WriteLine(Array.IndexOf(rakamlar,7));
+
+ 
