816341933fc6eba02466941694fd2fe3bff829c1
BerreYesilyurt <77548130+BerreYesilyurt@users.noreply.github.com>
2022-08-13 16:55:25 +0300

    Add files via upload

---
+++ metotlar/Program.cs
@@ -0,0 +1,30 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+int a=3;
+int b=2;
+
+string c=Console.ReadLine();
+
+Hello hello=new Hello();
+hello.EkranaYazdir(c);
+
+Topla(a,b);
+
+
+
+int Topla(int sayi1,int sayi2){
+
+    Console.WriteLine(a+b);
+
+    return a+b;
+
+}
+
+class Hello{
+    public void EkranaYazdir(string a){
+        Console.WriteLine(a);
+
+    }
+
+
+}
