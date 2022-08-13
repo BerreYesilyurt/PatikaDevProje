259bbe1ca1f1332a311b318d1bb58f1ea91d9705
BerreYesilyurt <77548130+BerreYesilyurt@users.noreply.github.com>
2022-08-13 16:46:50 +0300

    Add files via upload

---
+++ donguler1/Program.cs
@@ -0,0 +1,37 @@
+﻿// See https://aka.ms/new-console-template for more information
+int sayac=int.Parse(Console.ReadLine());
+
+int tekToplam=0;
+int ciftToplam=0;
+
+
+for(int i=0;i<=sayac;i++){
+
+    if(i%2==0){
+        Console.WriteLine(i+" - Çift Sayı");
+        ciftToplam+=i;
+
+    }
+
+    else{
+        Console.WriteLine(i+"  Tek Sayı");
+        tekToplam+=i;
+    }
+
+}
+
+Console.WriteLine("Tek sayıların toplamı : "+tekToplam);
+Console.WriteLine("Çift sayıların toplamı : "+ciftToplam);
+
+int sayi=int.Parse(Console.ReadLine());
+
+for(int j=0;j<sayi;j++){
+
+    if(j==2){
+        continue;
+    }
+    Console.WriteLine(j);
+}
+
+
+
