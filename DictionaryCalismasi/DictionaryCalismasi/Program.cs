581db0815d0634a2904ee2d8c02f2de0030579da
BerreYesilyurt <77548130+BerreYesilyurt@users.noreply.github.com>
2022-08-14 18:13:13 +0300

    Add files via upload

---
+++ DictionaryCalismasi/DictionaryCalismasi/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace DictionaryCalismasi
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Dictionary<int, string> kullanicilar = new Dictionary<int, string>();
+
+            kullanicilar.Add(1, "Hello World");
+            kullanicilar.Add(2, "Merhaba Dünya");
+
+            Console.WriteLine(kullanicilar[2]);
+
+
+
+            Console.WriteLine(kullanicilar.Count);
+
+            Console.WriteLine(kullanicilar.ContainsKey(1));
+            Console.WriteLine(kullanicilar.ContainsValue("hello"));
+
+            kullanicilar.Remove(1);
+
+            foreach (var yazi in kullanicilar)
+            {
+                Console.WriteLine(yazi);
+            }
+        }
+    }
+}
