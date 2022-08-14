1168ea503a00a9c91bf139d3ba70d93cee95501a
BerreYesilyurt <77548130+BerreYesilyurt@users.noreply.github.com>
2022-08-14 18:09:59 +0300

    Add files via upload

---
+++ ArrayListCalisma/ArrayListCalisma/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+
+namespace ArrayListCalisma
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ArrayList liste = new ArrayList();
+
+            liste.Add("Merhaba");
+            liste.Add(7);
+            liste.Add(true);
+
+            Console.WriteLine(liste[2]);
+
+            string[] renkler = {"Beyaz","Yeşil","Mavi"};
+
+            List<object> degerler = new List<object>() { 1, 2, 3, "Hello", "world", 4, 5, 6, 7 };
+
+            liste.AddRange(renkler);
+            liste.AddRange(degerler);
+
+            foreach(var deger in liste)
+            {
+                Console.WriteLine(deger);
+            }
+
+        }
+    }
+}
