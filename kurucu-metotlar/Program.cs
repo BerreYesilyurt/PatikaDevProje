6506a608bc5c865a5272124f0ee5635be5af9d18
BerreYesilyurt <77548130+BerreYesilyurt@users.noreply.github.com>
2022-09-11 15:12:22 +0300

    Add files via upload

---
+++ kurucu-metotlar/Program.cs
@@ -0,0 +1,48 @@
+﻿
+/*Calisan calisan1=new Calisan("Berre","Yeşilyurt",2054,"Bilgisayar Mühendisliği");
+calisan1.CalisaninBilgileri();*/
+
+/*calisan1.ad="Berre";
+calisan1.soyad="Yeşilyurt";
+calisan1.no=23;
+calisan1.departman="Bilişim";
+
+calisan1.CalisaninBilgileri();*/
+
+Calisan calisan1=new Calisan("Berre","Yeşilyurt");
+calisan1.CalisaninBilgileri();
+
+
+
+
+class Calisan{
+
+    public string ad;
+    public string soyad;
+    public int no;
+    public string departman;
+
+    public Calisan(string Ad, string Soyad, int No, string Departman)
+    {
+        this.ad=Ad;
+        this.soyad=Soyad;
+        this.no=No;
+        this.departman=Departman;
+    }
+
+    public Calisan(string Ad, string Soyad)
+    {
+        this.ad=Ad;
+        this.soyad=Soyad;
+    }
+
+    public Calisan() { }
+
+    public void CalisaninBilgileri(){
+
+        Console.WriteLine("Çalışanın Adı : {0}",ad);
+        Console.WriteLine("Çalışanın Soyadı : {0}",soyad);
+        Console.WriteLine("Çalışanın Numarası : {0}",no);
+        Console.WriteLine("Çalışanın Departmanı : {0}",departman);
+    }
+}
