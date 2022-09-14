235dbf92792e9489a7bac575202a97fabca9d5cf
BerreYesilyurt <77548130+BerreYesilyurt@users.noreply.github.com>
2022-09-14 13:02:10 +0300

    Create SQL-Odev4.sql

---
+++ SQL-Odev4.sql
@@ -0,0 +1,5 @@
+select distinct replacement_cost from film;
+SELECT COUNT(DISTINCT replacement_cost) FROM film;
+SELECT COUNT(*) FROM film WHERE title LIKE 'T%' AND rating='G';
+SELECT count(*) FROM country WHERE country LIKE '_____';
+SELECT COUNT(*) FROM city WHERE city ILIKE '%r';
