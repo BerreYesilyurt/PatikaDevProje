fb893244c41cad4a1318e8a58658cb31e5d7464c
BerreYesilyurt <77548130+BerreYesilyurt@users.noreply.github.com>
2022-09-14 17:54:57 +0300

    Add files via upload

---
+++ static-sinif/Program.cs
@@ -0,0 +1,55 @@
+﻿Console.WriteLine("Çalışan Sayısı: ",Calisan.CalisanSayisi);
+
+Calisan calisan=new Calisan("Berre","Yeşilyurt","Yazılım");
+
+Console.WriteLine("Çalışan Sayısı: ",Calisan.CalisanSayisi);
+
+
+Islemler.Topla(5,3);
+Islemler.Cikart(5,3);
+
+
+
+class Calisan{
+
+    private static int calisanSayisi;
+
+    public static int CalisanSayisi{
+        get {return calisanSayisi;}
+    }
+
+    private string isim;
+    private string soyisim;
+    private string departman;
+
+    static Calisan(){ 
+        // Static kurucuların erişim belirteçleri olmaz.
+        calisanSayisi=0;
+
+
+    }
+
+    public Calisan(string isim, string soyisim,string departman){
+        this.isim=isim;
+        this.soyisim=soyisim;
+        this.departman=departman;
+        calisanSayisi++;
+    }
+
+}
+
+static class Islemler{
+    public static long Topla(int sayi1, int sayi2){
+        Console.WriteLine(sayi1+sayi2);
+        return sayi1+sayi2;
+
+        
+    }
+
+    public static long Cikart(int sayi1, int sayi2){
+        Console.WriteLine(sayi1-sayi2);
+        return sayi1-sayi2;
+
+    }
+
+}
