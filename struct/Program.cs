e6e1c7c4f2e648489b5c00a4d785edf008a48327
BerreYesilyurt <77548130+BerreYesilyurt@users.noreply.github.com>
2022-09-14 22:43:42 +0300

    Add files via upload

---
+++ struct/Program.cs
@@ -0,0 +1,33 @@
+﻿Dikdortgen dikdortgen=new Dikdortgen();
+dikdortgen.kisaKenar=3;
+dikdortgen.uzunKenar=4;
+
+Console.WriteLine("Class Alan Hesabı : ",dikdortgen.alanHesapla());
+
+//Dikdortgen_Struct dikdortgen_struct=new Dikdortgen_Struct();
+Dikdortgen_Struct dikdortgen_struct; // new yazmadan bu şekilde de tanımlanabilir.
+
+dikdortgen_struct.kisaKenar=3;
+dikdortgen_struct.uzunKenar=4;
+
+Console.WriteLine("Struct alan hesabı : ",dikdortgen_struct.alanHesapla());
+
+
+
+class Dikdortgen{
+    public int kisaKenar;
+    public int uzunKenar;
+
+    public long alanHesapla(){
+        return this.kisaKenar*this.uzunKenar;
+    }
+}
+
+struct Dikdortgen_Struct{
+    public int kisaKenar;
+    public int uzunKenar;
+
+    public long alanHesapla(){
+        return this.kisaKenar*this.uzunKenar;
+    }
+}
