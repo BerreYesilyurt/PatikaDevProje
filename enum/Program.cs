cdcd5c6c6f262f7809945926550bf38ee20bda65
BerreYesilyurt <77548130+BerreYesilyurt@users.noreply.github.com>
2022-09-14 23:00:58 +0300

    Add files via upload

---
+++ enum/Program.cs
@@ -0,0 +1,27 @@
+﻿
+
+Console.WriteLine(Gunler.Pazar);
+Console.WriteLine((int)Gunler.Cumartesi);
+
+int sicaklik=Convert.ToInt32(Console.ReadLine());
+
+if(sicaklik<=(int)havaDurumu.Normal){
+    Console.WriteLine("Hava soğuk");
+}
+enum Gunler{
+    Pazartesi=1, 
+    Salı,
+    Çarşamba,
+    Perşembe,
+    Cuma,
+    Cumartesi,
+    Pazar
+}
+
+enum havaDurumu{
+    Soguk=5,
+    Normal=20,
+    Sıcak=25,
+    ÇokSıcak=30
+
+}
